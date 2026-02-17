e5050a5f33c9e4affffbb72a214917806ba4a3aa
DoctorTangerina <arthursardellajiujitsu@gmail.com>
2026-02-16 23:49:47 -0300

    Changed to a simpler model for testing

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-from transformers import pipeline
-import torch
-import os
-os.environ['HF_HOME'] = 'D:\\puc\\TCC\\.cache'
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_id = "openai/gpt-oss-20b"
+model_id = "Qwen/Qwen3-0.6B"
 
-pipe = pipeline(
-    "text-generation",
-    model=model_id,
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
     torch_dtype="auto",
-    device_map="auto",
+    device_map="auto"
 )
-
+prompt = "Explain the basics of quantum physics."
 messages = [
-    {"role": "user", "content": "Explain quantum mechanics clearly and concisely."},
+    {"role": "user", "content": prompt}
 ]
-
-outputs = pipe(
+text = tokenizer.apply_chat_template(
     messages,
-    max_new_tokens=256,
+    tokenize=False,
+    add_generation_prompt=True,
+    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
 )
-print(outputs[0]["generated_text"][-1])
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+# conduct text completion
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=32768
+)
+output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist()
+
+# parsing thinking content
+try:
+    # rindex finding 151668 (</think>)
+    index = len(output_ids) - output_ids[::-1].index(151668)
+except ValueError:
+    index = 0
+
+thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
+content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+print("thinking content:", thinking_content)
+print("content:", content)
